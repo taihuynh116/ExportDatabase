56e78f293a082e9f0f2dab4d36c3d7140a935428
taihuynh116 <hthientai@gmail.com>
2018-06-14 03:09:28 +0700

    13

---
+++ ExportDatabase/WpfApp1/MyCustomClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    class MyCustomClass
+    {
+        private ICommand _saveCommand;
+        public ICommand SaveCommand
+        {
+            get
+            {
+                if (_saveCommand == null)
+                {
+                    _saveCommand = new RelayCommand(
+                        param => this.SaveObject(),
+                        param => this.CanSave()
+                        );
+                }
+                return _saveCommand;
+            }
+        }
+        public bool CanSave()
+        {
+            return true;
+        }
+        public void SaveObject()
+        {
+            MessageBox.Show("OK");
+        }
+    }
+}
