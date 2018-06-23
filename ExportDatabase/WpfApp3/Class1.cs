e3e8893e6a46098b41a1d0c752ddb5ac024115d3
Huynh Thien Tai <hthientai@gmail.com>
2018-06-23 09:10:32 +0700

    1

---
+++ ExportDatabase/WpfApp3/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    class Class1
+    {
+        public int A { get; set; }
+    }
+    class Command
+    {
+        public void Excecute()
+        {
+            Class1 ban1 = new Class1();
+            ban1.A = 3;
+            Class1 ban2 = new Class1();
+            ban2.A = 4;
+        }
+    }
+}
